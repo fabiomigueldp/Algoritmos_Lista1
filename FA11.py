a6874edab4e62bebd84ee8ba519e0f0021bcf9a0
Fabio Miguel <fabiomigueldp@Mac-mini-de-Fabio.local>
2023-03-26 01:35:08 -0300

    Comentário adicionado e arquivo removido

--- FA11.py
+++ FA11.py
@@ -10,3 +10,4 @@ lon2 = float(input("Insira a longitude do ponto 2: "))
 haversine = 6371.01 * math.acos(math.sin(lat1)*math.sin(lat2)+math.cos(lat1)*math.cos(lat2)*math.cos(lon1-lon2))
 #A Fórmula de Haversine está incorreta.
 print(f"A distância entre os pontos inseridos, é de {haversine}km.")
+print("teste")
