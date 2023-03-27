dd7028b62d796d68477873226bf6de41750b60bc
fabiomigueldp <fabiomigueldp@gmail.com>
2023-03-27 08:14:24 -0300

    Upload da atividade 12 e algumas outras correções.

---
+++ FA12.py
@@ -0,0 +1,26 @@
+import math
+
+# Definindo as funções
+
+def area_circulo(raio):
+    ac = math.pi*raio**2
+    return ac
+
+def volume_esfera(raio):
+    ve = 4/3*math.pi*raio**3
+    return ve 
+
+
+# Solicita o raio
+
+raio = float(input("Insira um valor para o raio em uma unidade X: "))
+
+# Calcula a área e o volume
+area = area_circulo(raio)
+
+volume = volume_esfera(raio)
+
+# Imprime a área e o volume
+
+print(f"A área de uma circunferência de raio {raio} é de {area: .2f}X.")
+print(f"O volume de uma esfera de raio {raio} é de {volume: .2f}X.")
